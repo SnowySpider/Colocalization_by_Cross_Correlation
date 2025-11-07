dd392d468d3d4801fc96de2d76b8ac7d03c9955b
andmccall <amccall@buffalo.edu>
2025-11-07 14:13:04 -0500

    More changes to hopefully fix Maven build error.

--- src/main/java/utils/ErrorChecking.java
+++ src/main/java/utils/ErrorChecking.java
@@ -2,7 +2,7 @@
  * #%L
  * Scijava plugin for spatial correlation
  * %%
- * Copyright (C) 2019 - 2024 Andrew McCall, University at Buffalo
+ * Copyright (C) 2019 - 2025 Andrew McCall, University at Buffalo
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
